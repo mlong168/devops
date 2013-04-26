560211d83ef231cdd4723d43f2ee66ae94a94755
ZHANG Cheng <zhangcheng@appannie.com>
2013-04-26 21:47:09 +0800

    not set global git config

--- vagrant/salt/roots/salt/etckeeper/etckeeper-init.sh
+++ vagrant/salt/roots/salt/etckeeper/etckeeper-init.sh
@@ -5,8 +5,6 @@ if [ -d .git ]; then
 	echo "etckeeper already inited"
 else
 	etckeeper init
-	git config --global user.name "Ruoogle Ops"
-	git config --global user.email "ops@mamabang.com"
 	etckeeper commit -m "initial commit"
 	echo "etckeeper inited"
 fi
